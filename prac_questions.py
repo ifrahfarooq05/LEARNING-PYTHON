00804fab410cefdbd3ec8a62fbbcbfa6f690fbfb
ifrahfarooq05 <ifrahfarooq95@gmail.com>
2025-08-04 18:51:53 +0500

    Added some practice questions

--- prac_questions.py
+++ prac_questions.py
@@ -8,8 +8,28 @@ def prime(num):
             return False
     return True    
 
-a = int(input("Enter a number: "))
+#a = int(input("Enter a number: "))
+a= 20
 print(f"Prime numbers upto {a}: ")        
 for i in range(2,a+1):
     if prime(i):
         print(i, end=',')
+
+#Print numbers from 1 to 50, but skip numbers divisible by 3. 
+for n in range(1,51):
+    if n%3==1:
+        print(n)
+
+#Ask the user to enter a word and print:
+#The word in uppercase #The first and last letter #The word in reverse
+#Count how many times the letter 'a' appears in a string.
+def print_str (s):
+    print(s.upper())#uppercase
+    print(s[0]) #first letter
+    print(s[-1])#last letter
+    print(s[::-1])#reverse text
+    print(s.count('a'))#count a
+
+#b = input("Enter a sentence: ") 
+#print_str(b)   
+
